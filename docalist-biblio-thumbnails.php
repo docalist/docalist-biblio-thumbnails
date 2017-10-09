9cd47d4df5bffa9e5f7928a7f72986b642919fb7
Daniel Ménard <daniel.menard@laposte.net>
2017-10-09 20:33:10 +0200

    Définit une constante pour l'url du plugin.

--- docalist-biblio-thumbnails.php
+++ docalist-biblio-thumbnails.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the 'Docalist Biblio' plugin.
  *
- * Copyright (C) 2012-2015 Daniel Ménard
+ * Copyright (C) 2012-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
@@ -23,12 +23,35 @@
 
 namespace Docalist\Biblio\Thumbnails;
 
-// Définit une constante pour indiquer que ce plugin est activé
-define('DOCALIST_BIBLIO_THUMBNAILS', __DIR__);
+/**
+ * Version du plugin.
+ */
+define('DOCALIST_BIBLIO_THUMBNAILS_VERSION', '0.3.0'); // Garder synchro avec la version indiquée dans l'entête
+
+/**
+ * Path absolu du répertoire dans lequel le plugin est installé.
+ *
+ * Par défaut, on utilise la constante magique __DIR__ qui retourne le path réel du répertoire et résoud les liens
+ * symboliques.
+ *
+ * Si le répertoire du plugin est un lien symbolique, la constante doit être définie manuellement dans le fichier
+ * wp_config.php et pointer sur le lien symbolique et non sur le répertoire réel.
+ */
+!defined('DOCALIST_BIBLIO_THUMBNAILS_DIR') && define('DOCALIST_BIBLIO_THUMBNAILS_DIR', __DIR__);
+
+/**
+ * Path absolu du fichier principal du plugin.
+ */
+define('DOCALIST_BIBLIO_THUMBNAILS', DOCALIST_BIBLIO_THUMBNAILS_DIR . DIRECTORY_SEPARATOR . basename(__FILE__));
+
+/**
+ * Url de base du plugin.
+ */
+define('DOCALIST_BIBLIO_THUMBNAILS_URL', plugins_url('', DOCALIST_BIBLIO_THUMBNAILS));
 
 /**
  * Initialise le plugin.
-*/
+ */
 add_action('plugins_loaded', function () {
     // Auto désactivation si les plugins dont on a besoin ne sont pas activés
     $dependencies = ['DOCALIST_CORE', 'DOCALIST_BIBLIO'];
